09605b66af417b65e986abca9eb7b7d11d7b4ac8
Jonathan Morrison <jonathanmorrison@Jonathans-MacBook-Pro.local>
2019-04-13 23:39:05 +0100

    Accesses the test data folder and outputs a list attributes.

--- Scripts/file_name_dictionary.py
+++ Scripts/file_name_dictionary.py
@@ -1,5 +1,32 @@
 """Function to return a dict of filename and market for files in 'Data'."""
 
+import bz2
+import json
+# import datetime
+# import glob
+# import pandas as pd
+# import numpy as np
+import os
+# from os import listdir
+# from os.path import isfile, join
 
-def file_dict():
-    """Create dict of filenames and markets from files in 'Data'."""
+
+def data_indexer():
+    """Create and index of filenames and markets from files in 'Data'."""
+    path = os.getcwd()+'\\Test_Data'
+    index_list = []
+
+    for file in os.listdir(path):
+        with bz2.open(path+"\\"+file, 'r') as f:
+            data = f.read().decode('utf-8')
+        dict_list = [d.strip() for d in data.splitlines()]
+        data = [json.loads(i) for i in dict_list]
+
+        id = data[-1]['mc'][0]['id']
+        eventId = data[-1]['mc'][0]['marketDefinition']['eventId']
+        eventName = data[-1]['mc'][0]['marketDefinition']['eventName']
+        openDate = data[-1]['mc'][0]['marketDefinition']['openDate']
+
+        index_list.append([id, eventId, eventName, openDate])
+
+    print(index_list)
---
+++ main.py
@@ -0,0 +1,5 @@
+"""Main function platform, all scripts should be called from here."""
+
+from Scripts.file_name_dictionary import data_indexer
+
+data_indexer()
