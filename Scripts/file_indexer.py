ad150ab2b9f3c132e372e81d61afd0d1f245c736
monstrouspoon <jonjmorrison@gmail.com>
2019-04-17 20:37:07 +0100

    Extracts everything; throws a UTF-8 error.

--- Scripts/file_indexer.py
+++ Scripts/file_indexer.py
@@ -4,6 +4,7 @@ import bz2
 import json
 import pandas as pd
 import os
+import sys
 
 
 def file_indexer():
@@ -37,12 +38,14 @@ def file_indexer():
                                openDate, marketTime, settledTime])
             print(round((itera/len(os.listdir(path)))*100, 2), "%")
             itera += 1
+            print(file)
 
         except Exception as err:
             errors.append([file, err])
             print("An error has occured in filename: ", file)
             print(err)
             itera += 1
+            print(file)
             pass
 
     with open("0_errors.txt", "w") as output:
@@ -53,3 +56,71 @@ def file_indexer():
 
     index_df = pd.DataFrame(index_list, columns=columns)
     index_df.to_csv('Output\\File_index.csv', index=False)
+
+
+def file_list(folder, chunk_size):
+    """Create and index of filenames and markets from files in 'Data'."""
+    output_exists = os.path.isfile('Output\\output.csv')
+    if output_exists is True:
+        print("+++++++++++++++++++++++++++++++++")
+        print("  OUTPUT FILE ALREADY EXISTS     ")
+        print(" DELETE OUTPUT FILE TO CONTINUE  ")
+        print("+++++++++++++++++++++++++++++++++")
+        sys.exit()
+
+    path = os.getcwd()+'\\'+folder
+    file_list = os.listdir(path)
+    file_itr = iter(file_list)
+    columns = ['Filename', 'Event ID', 'Game', 'Country Code', 'Market',
+               'Market Name', 'Market Time', 'Settlement Time']
+
+    index_list = []
+    chunk_no = 1
+
+    for i in range(len(file_list)):
+        file = next(file_itr)
+        with bz2.open(path+"\\"+file, 'r') as f:
+            data = f.read().decode('utf-8')
+        dict_list = [d.strip() for d in data.splitlines()]
+        data = [json.loads(i) for i in dict_list]
+
+        id = data[-1]['mc'][0]['id']
+        eventId = data[-1]['mc'][0]['marketDefinition']['eventId']
+        eventName = data[-1]['mc'][0]['marketDefinition']['eventName']
+        countryCode = data[-1]['mc'][0]['marketDefinition']['countryCode']
+
+        marketType = data[-1]['mc'][0]['marketDefinition']['marketType']
+        name = data[-1]['mc'][0]['marketDefinition']['name']
+
+        marketTime = data[-1]['mc'][0]['marketDefinition']['marketTime']
+        settledTime = data[-1]['mc'][0]['marketDefinition']['settledTime']
+
+        if i % chunk_size == 0 and i > 0:
+            # print(file)
+            index_list.append([id, eventId, eventName, countryCode,
+                               marketType, name,
+                               marketTime, settledTime])
+
+            temp_df = pd.DataFrame(index_list)
+            with open('Output\\output.csv', 'a', newline='') as f:
+                temp_df.to_csv(f, header=False, index=False)
+
+            index_list = []
+            print("Chunk number ", chunk_no, " finished: ",
+                  round(((chunk_no*chunk_size)/len(file_list))*100, 2),
+                  "% completed.")
+            chunk_no += 1
+
+        else:
+            # print(file)
+            index_list.append([id, eventId, eventName, countryCode,
+                               marketType, name,
+                               marketTime, settledTime])
+
+    temp_df = pd.DataFrame(index_list)
+    with open('Output\\output.csv', 'a', newline='') as f:
+        temp_df.to_csv(f, header=False, index=False)
+
+    df = pd.read_csv('Output\\output.csv', index_col=None, names=columns)
+    df.to_csv('Output\\output.csv', index=False)
+    print('Done')
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Main function platform, all scripts should be called from here."""
 
-from Scripts.file_indexer import file_indexer
+from Scripts.file_indexer import file_indexer, file_list
 
-file_indexer()
+# file_indexer()
+file_list('Data', 100)
